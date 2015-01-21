5d8181d188cdee839046e79eb14807ad97c272c2
Bilal Itani <bilalitani1@gmail.com>
2015-01-21 09:50:14 -0500

    sync

--- Main.py
+++ Main.py
@@ -24,31 +24,20 @@ vowels = ['a', 'e', 'i', 'o', 'u']
 consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v',
               'w', 'x', 'z', 'y']
 
+letterProbabilities = dict([('a', 0), ('b', 0), ('c', 0), ('d', 0), ('e', 0), ('f', 0), ('g', 0), ('h', 0), ('i', 0), ('j', 0),
+                ('k', 0), ('l', 0), ('m', 0), ('n', 0), ('o', 0), ('p', 0), ('q', 0), ('r', 0), ('s', 0), ('t', 0),
+                ('u', 0), ('v', 0), ('w', 0), ('x', 0), ('y', 0), ('z', 0)])
+
 
 def function():
     length = random.randrange(4, 8)
     "print(length)"
     word = ''
     for i in range(0, length):
+        value = random()
         if i == 0:
             letter = letters[random.randrange(0, 25)]
         elif isconsonant(letter):
-            letter = vowels[random.randrange(0, len(vowels))]
-        elif isvowel(letter):
-            letter = consonants[random.randrange(0, len(consonants))]
-        word += letter
-    print(word + '\n')
-    return None
-
-
-def function():
-    length = random.randrange(4, 8)
-    "print(length)"
-    word = ''
-    for i in range(0, length):
-        if i == 0:
-            letter = letters[random.randrange(0, 25)]
-        elif letter == 'a':
             letter = vowels[random.randrange(0, len(vowels))]
         elif isvowel(letter):
             letter = consonants[random.randrange(0, len(consonants))]
@@ -67,14 +56,31 @@ def sort(dictionary):
     i = 0
     j = i + 1
     while i < len(sorted):
-        i = i + 1
+        i += 1
         while j < len(sorted):
-            j = j + 1
+            j += 1
             if sorted[i] > sorted[j] and i != j:
                 temp = sorted[j]
                 sorted[j] = sorted[i]
                 sorted[i] = temp
     return sorted
+
+
+def sort2(dictionary, array, randomValue):
+    sorted = array
+
+    i = 0
+    j = i + 1
+    while i < len(sorted):
+        i += 1
+        while j < len(sorted):
+            j += 1
+            if dictionary[sorted[i]] < dictionary[sorted[j]] and i != j:
+                temp = dictionary[sorted[j]]
+                dictionary[sorted[j]] = dictionary[sorted[i]]
+                dictionary[sorted[i]] = temp
+    return sorted
+
 
 def nextletter(letter):
     if letter == 'a':
